2b951e5f696b80735af1e8a69938690c9361bc3d
jmanjarresm <124469422+jmanjarresm@users.noreply.github.com>
2024-03-13 16:27:12 -0500

    Update form.js

--- form.js
+++ form.js
@@ -8,45 +8,12 @@ document.addEventListener('DOMContentLoaded', () => {
         
         const us = document.getElementById('us').value;
         const pw = document.getElementById('pw').value;
-        if (!us || !pw) {
-            alertify.error('Es requerido los datos de usuario y clave');
-            return
+        
+        if (us === 'smartcalidad' && pw === 'temporal') {
+            localStorage.setItem('auth', "ok");
+            window.location.href = 'chatbot.html';
+        } else {
+            alertify.error('Usuario o contraseña incorrectos');
         }
-
-        const headers = new Headers();
-        headers.append("Content-Type", "application/json");
-
-        const raw = JSON.stringify({
-            "us": us,
-            "pw": pw
-        });
-
-        const requestOptions = {
-            method: "POST",
-            headers: headers,
-            body: raw,
-            redirect: "follow"
-        };
-
-        fetch("http://msvc-auth:8002/auth-prinx/auth", requestOptions)
-            .then((response) => response.text())
-            .then((result) => {
-
-                if (result) {
-
-                    const user = JSON.parse(result)
-
-                    if (Object.entries(user).some(([key, value]) => key === 'ERROR')) {
-                        alertify.error(user.ERROR)
-                        return
-                    }
-
-                    if (user.auth === 'ok') {
-                        localStorage.setItem('auth', "ok")
-                        window.location.href = 'chatbot.html';
-                    }
-
-                }
-            }).catch((error) => alertify.error('Usuario o clave invalida'));
     });
 });
